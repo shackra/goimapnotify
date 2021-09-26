bc9567d1aa00fe0f562a68b31f2e642675ff26de
Jorge Javier Araya Navarro <jorge@esavara.cr>
2021-09-26 17:11:42 -0600

    chg: Update years

--- config.go
+++ config.go
@@ -1,5 +1,21 @@
 package main
 
+// This file is part of goimapnotify
+// Copyright (C) 2017-2021  Jorge Javier Araya Navarro
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // NotifyConfig holds the configuration
 type NotifyConfig struct {
 	Host       string `json:"host"`
